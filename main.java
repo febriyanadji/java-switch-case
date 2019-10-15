7b264e964879a5eb723cb1bdcca93e876c87830e
febri <febriyan.aji@gmail.com>
2019-10-15 14:02:18 +0700

    started

---
+++ main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int bil;
+        System.out.print("Masukkan bilangan = ");
+        bil = in.nextInt();
+        switch(bil)
+        {
+            case 1:
+                System.out.println("Ini 1");
+            break;
+            case 2:
+                System.out.println("Ini 2");
+            break;
+            default:
+                System.out.println("Ini tidak sama sekali");
+            break;
+        }
+    }
+}
